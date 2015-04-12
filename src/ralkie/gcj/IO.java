6c8bf07dc6b7e631614e9dcaa034a0cf5e95123f
Kasparas <kasparas.rudokas@gmail.com>
2015-04-13 00:11:28 +0300

    casers

--- src/ralkie/gcj/IO.java
+++ src/ralkie/gcj/IO.java
@@ -1,6 +1,7 @@
 package ralkie.gcj;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,105 +10,141 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static ralkie.gcj.Converter.*;
+
 public class IO {
 
     public static final String DEFAULT_IN = "in";
     public static final String DEFAULT_OUT = "out";
 
-    public static Data read(String filename) throws IOException {
-        Data data = new Data();
-        try(BufferedReader reader = Files.newBufferedReader(Paths.get(filename))){
+    public static Dataset read(String filename) throws IOException {
+        Dataset dataset = new Dataset();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filename))) {
             String line;
             while ((line = reader.readLine()) != null)
-                data.add(line);
+                dataset.add(line);
         }
-        return data;
+        return dataset;
     }
 
-    public static Data read() throws IOException {
+    public static Dataset read() throws IOException {
         return read(DEFAULT_IN);
     }
 
-    public static void write(String filename, List<String> results){
-
+    public static void write(String filename, List<String> results) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filename))) {
+            int caseCnt = 1;
+            for (String result : results) {
+                writer.write(String.format("Case #%d: %s", caseCnt++, result));
+            }
+        }
     }
 
-    public static void write(List<String> results){
+    public static void write(List<String> results) throws IOException {
         write(DEFAULT_OUT, results);
     }
 }
 
-class Data {
+class Dataset {
     public static final String DEFAULT_SEPARATOR = " ";
-    List<List<String>> data = new ArrayList<>();
+    List<List<String>> rows = new ArrayList<>();
 
-    public int size(){
-        return data.size();
+    public int size() {
+        return rows.size();
     }
 
-    public void add(String s){
-        data.add(Arrays.asList(s.split(DEFAULT_SEPARATOR)));
+    public void add(String s) {
+        rows.add(Arrays.asList(s.split(DEFAULT_SEPARATOR)));
     }
 
-    public void add(List<String> t){
-        data.add(t);
+    public void add(List<String> t) {
+        rows.add(t);
     }
 
-    public String get(int row, int col){
-        return data.get(row).get(col);
+    public void addAll(List<List<String>> rows) {
+        this.rows.addAll(rows);
     }
 
-    public List<String> getAll(int row){
-        return data.get(row);
+    public String get(int row, int col) {
+        return rows.get(row).get(col);
     }
 
-    public List<String> getAll(int row, int fromCol) {
-        return getAll(row, fromCol, data.get(row).size());
+    public List<String> getRow(int row) {
+        return rows.get(row);
     }
 
-    public List<String> getAll(int row, int fromCol, int toCol){
-        return data.get(row).subList(fromCol, toCol);
+    public List<String> getRow(int row, int fromCol) {
+        return getRow(row, fromCol, rows.get(row).size());
     }
 
+    public List<String> getRow(int row, int fromCol, int toCol) {
+        return rows.get(row).subList(fromCol, toCol);
+    }
+
+    public Dataset subDataset(int fromRow, int toRow) {
+        Dataset sub = new Dataset();
+        sub.addAll(rows.subList(fromRow, toRow));
+        return sub;
+    }
 }
 
-interface Caser{
-    List<Data> getCases();
+interface Caser {
+    List<Dataset> getCases();
 }
 
-class FixedCaser implements Caser{
-    private Data all;
+class FixedCaser implements Caser {
+    private Dataset dataset;
     private int caseSize;
-    public FixedCaser(Data all, int caseSize) {
-        this.all = all;
+
+    public FixedCaser(Dataset dataset, int caseSize) {
+        this.dataset = dataset;
         this.caseSize = caseSize;
     }
 
-    public List<Data> getCases(){
-        return null; //todo
+    public List<Dataset> getCases() {
+        List<Dataset> cases = new ArrayList<>();
+        int caseCnt = toI(dataset.get(0, 0));
+        for (int i = 0; i < caseCnt; i++) {
+            int fromRow = i * caseSize + 1;
+            int toRow = fromRow + caseSize;
+            cases.add(dataset.subDataset(fromRow, toRow));
+        }
+        return cases;
     }
 }
 
-class FloatingCaser implements Caser{
-    private Data all;
+class FloatingCaser implements Caser {
+    private Dataset dataset;
 
-    public FloatingCaser(Data all) {
-        this.all = all;
+    public FloatingCaser(Dataset dataset) {
+        this.dataset = dataset;
     }
 
     @Override
-    public List<Data> getCases() {
-        return null; //todo
+    public List<Dataset> getCases() {
+        List<Dataset> cases = new ArrayList<>();
+        int i = 1;
+        while (i < dataset.size()) {
+            int caseSize = toI(dataset.get(i, 0));
+            int fromRow = i;
+            int toRow = fromRow + caseSize;
+            cases.add(dataset.subDataset(fromRow, toRow));
+        }
+        return cases;
     }
 }
 
 
 class Converter {
-    public static Integer toI(String s){
+    public static Integer toI(String s) {
         return Integer.valueOf(s);
     }
 
-    public static Double toD(String s){
+    public static Long toL(String s) {
+        return Long.valueOf(s);
+    }
+
+    public static Double toD(String s) {
         return Double.valueOf(s);
     }
 
@@ -117,6 +154,10 @@ class Converter {
 
     public static List<Integer> toIL(List<String> list) {
         return list.stream().map(Integer::valueOf).collect(Collectors.toList());
+    }
+
+    public static List<Long> toLL(List<String> list) {
+        return list.stream().map(Long::valueOf).collect(Collectors.toList());
     }
 
     public static List<Double> toDL(List<String> list) {
